52586e1863da70171519e4d7163a0791ce0b1a8b
serg-e <smosesov@gmail.com>
2019-04-16 08:33:55 +0100

    bar chart added

--- web-app/commons-vis.js
+++ web-app/commons-vis.js
@@ -9,19 +9,23 @@ let data2 = [
         party: 'Conservative',
         ayes: 200,
         noes: 50,
+        color:"#135789",
 
     },
     {
         party: 'Labour',
         ayes: 100,
         noes: 150,
+        color: "#E0242E"
     }
 ];
 
 
+const swidth = 1000 ;
+const sheight = 500 ;
 const margin = 100;
-const width = 1000 - 2 * margin;
-const height = 600 - 2 * margin;
+const width = swidth - 2 * margin;
+const height = sheight - 2 * margin;
 
 
 const maxY = Math.max(...data2.map(d => d.ayes));
@@ -29,21 +33,23 @@ const maxY = Math.max(...data2.map(d => d.ayes));
 console.log(maxY)
 
 const svg = d3.select('svg')
-              .attr('width',1000)
-              .attr('height',600);
+              .attr('width',swidth)
+              .attr('height',sheight);
 
 const chart = svg.append('g')
                  .attr('transform', `translate(${margin},${margin})`);
 
-const yScale = d3.scaleLinear()
-                 .range([height,0])
+const xScale = d3.scaleLinear()
+                 .range([0,width])
                  .domain([0,maxY]);
+
+const yScale = d3.scaleBand()
+                .range([height, 0])
+                .domain(data2.map(d => d.party));
 
 chart.append('g').call(d3.axisLeft(yScale));
 
-const xScale = d3.scaleBand()
-                 .range([0,width])
-                 .domain(data2.map(d => d.party));
+
 
 chart.append('g')
      .attr('transform', `translate(0,${height})`)
@@ -55,18 +61,19 @@ chart.selectAll()
      .data(data2)
      .enter()
      .append('rect')
-     .attr('x', d => xScale(d.party))
-     .attr('y', d => yScale(d.ayes))
-     .attr('width', d => xScale.bandwidth()-50)
-     .attr('height', d => height - yScale(d.ayes))
+     .attr('x', d => 0)
+     .attr('y', d => yScale(d.party))
+     .attr('height', d => yScale.bandwidth())
+     .attr('width', d => xScale(d.ayes))
+     .attr ('fill', d => d.color)
 
 // add the grid lines
 
 chart.append('g')
      .attr('class', 'grid')
-     .call(d3.axisLeft()
-             .scale(yScale)
-             .tickSize(-width,0,0)
+     .call(d3.axisBottom()
+             .scale(xScale)
+             .tickSize(height,0,0)
              .tickFormat(''))
 
 svg.append('text')
@@ -77,9 +84,19 @@ svg.append('text')
 
 
 chart.selectAll('rect')
-      .on('mouseenter', function (actual, i) {
+     .on('mouseenter', function (d, i) {
           d3.select(this).attr('opacity', 0.5)
+
+          chart.append('line')
+               .attr('id', 'marker')
+               .attr('y1', 0 )
+               .attr('x1', xScale(d.ayes))
+               .attr('y2', height )
+               .attr('x2', xScale(d.ayes))
+               .attr('stroke', d.color)
       })
       .on('mouseleave', function (actual, i) {
           d3.select(this).attr('opacity', 1)
+          chart.selectAll('#marker')
+               .remove()
       })
